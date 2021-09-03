956925ef57aeb20991f8df07d23bd2702043f7cf
Grace Gardner <graceirisgardner@gmail.com>
2021-09-03 12:24:37 -0600

    Add variables

---
+++ variables.js
@@ -0,0 +1,13 @@
+var movie = "Surfs Up";
+var isEntertaining = true;
+
+movie = "Iron Man";
+movie.length();
+
+var num4 = 5;
+var num6 = 7;
+
+var product = num4 * num6;
+
+var favoriteFoods = ["burgers", "bbq", "ice cream"];
+favoriteFoods.shift();
