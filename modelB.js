b7d66ba83448f24ae338a2375b0bbbc264248078
Grace Gardner <graceirisgardner@gmail.com>
2021-09-03 12:46:51 -0600

    Add developer variable object

---
+++ modelB.js
@@ -0,0 +1,10 @@
+var Developer = {
+  devName:"Fred",
+  favoriteComputer:"Mac",
+  typingSpeedPerMin:90,
+  age:56,
+  isHappy:true,
+  isSleepy:false,
+  hobbies:["walks in the park", "singing in the forest"],
+  favoritePlaces:["home", "the park", "the forest"]
+}
