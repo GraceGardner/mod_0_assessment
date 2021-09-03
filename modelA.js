60c9f1b0b51041ca5daecf4c4fe1f11baaea795e
Grace Gardner <graceirisgardner@gmail.com>
2021-09-03 12:32:48 -0600

    Create dinosaur

---
+++ modelA.js
@@ -0,0 +1,10 @@
+var Dinosaur{
+  var name = "Sam";
+  var species = "T-Rex";
+  var speedInMPH = 50;
+  var age = 10;
+  var isHandsome = true;
+  var isTired = false;
+  var favoriteFoods = ["small dinosaurs", "big dinosaurs"];
+  var hobbies = ["chasing dinosaurs", "eating dinosaurs"];
+}
