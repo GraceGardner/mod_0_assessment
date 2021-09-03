7d1a5c6b6b64c0ff506aa2da114e7046423755bd
Grace Gardner <graceirisgardner@gmail.com>
2021-09-03 12:45:44 -0600

    Fix even more syntax errors

--- modelA.js
+++ modelA.js
@@ -1,10 +1,10 @@
 var Dinosaur = {
-  name:"Sam";
-  species:"T-Rex";
-  speedInMPH:50;
-  age:10;
-  isHandsome:true;
-  isTired:false;
-  favoriteFoods:["small dinosaurs", "big dinosaurs"];
-  hobbies:["chasing dinosaurs", "eating dinosaurs"];
+  name:"Sam",
+  species:"T-Rex",
+  speedInMPH:50,
+  age:10,
+  isHandsome:true,
+  isTired:false,
+  favoriteFoods:["small dinosaurs", "big dinosaurs"],
+  hobbies:["chasing dinosaurs", "eating dinosaurs"]
 }
